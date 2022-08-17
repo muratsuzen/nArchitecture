deb373cafcb76c909ef6f4ef3bc8bd560eb1925a
muratsuzen <muratsuzen@gmail.com>
2022-08-17 14:54:49 +0300

    1day

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,21 +1,15 @@
-using Application;
-using Core.CrossCuttingConcerns.Exceptions;
-using Infrastructure;
-using Microsoft.EntityFrameworkCore;
 using Persistence;
-using Persistence.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddApplicationServices();
-builder.Services.AddSecurityServices();
+builder.Services.AddControllers();
+//builder.Services.AddApplicationServices();
+//builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
-builder.Services.AddInfrastructureServices();
-builder.Services.AddHttpContextAccessor();
-
-
+//builder.Services.AddInfrastructureServices();
+//builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,9 +24,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-if (app.Environment.IsProduction())
-    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
