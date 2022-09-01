e5f81ea6ad3db31954050cb6e56666e55931c896
muratsuzen <muratsuzen@gmail.com>
2022-09-01 12:47:40 +0300

    Day 3 Added GetById Query

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommandValidatior.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Brands.Commands.CreateBrand;
+
+public class CreateBrandCommandValidatior:AbstractValidator<CreateBrandCommand>
+{
+    public CreateBrandCommandValidatior()
+    {
+        RuleFor(x=>x.Name).NotEmpty();
+        RuleFor(x => x.Name).MinimumLength(2);
+    }
+}
